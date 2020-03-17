4352cd0527cf37bea079c1849e4a25dca353213c
unknown <xiexiaoyang@dangdang.com>
2020-03-17 16:19:31 +0800

    feat: add long press event

---
+++ src/longPress/useLongPress.ts
@@ -0,0 +1,58 @@
+import { useRef, useCallback } from "react";
+
+interface Options {
+  isPreventDefault?: boolean,
+  delay?: number,
+}
+
+const isTouchEvent = (event: Event): event is TouchEvent => {
+  return "touches" in event;
+};
+
+const preventDefault = (event: Event) => {
+  if(!isTouchEvent(event)) return;
+
+  if(event.touches.length < 2 && event.preventDefault) {
+    event.preventDefault();
+  }
+};
+
+const useLongPress = (
+  callback: (e: TouchEvent | MouseEvent) => void,
+  { isPreventDefault = true, delay = 300 } : Options = {}
+) => {
+  const timeout = useRef<ReturnType<typeof setTimeout>>();
+  const target = useRef<EventTarget>();
+
+  const start = useCallback(
+    (event: TouchEvent | MouseEvent) => {
+      if(isPreventDefault && event.target) {
+        event.target.addEventListener('touchend', preventDefault, { passive: false })
+        target.current = event.target;
+      }
+      timeout.current = setTimeout(() => callback(event), delay);
+    },
+    [callback, delay]
+  );
+
+  const clear = useCallback(
+    () => {
+      timeout.current && clearTimeout(timeout.current);
+
+      if(isPreventDefault && target.current) {
+        target.current.removeEventListener('touchend', preventDefault);
+      }
+    },
+    [],
+  );
+
+  return {
+    onMouseDown: (e: any) => start(e),
+    onTouchStart: (e: any) => start(e),
+    onMouseUp: clear,
+    onMouseLeave: clear,
+    onTouchEnd: clear
+  } as const;
+};
+
+export default useLongPress;
